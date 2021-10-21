07000ddc0e11fa5229258c4af3ca41a18373fec7
fadilkun45 <indocrafter76@gmail.com>
2021-10-21 21:21:44 +0700

    safepoint part 2

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGODB: 'mongodb+srv://fadil:TFM3UH0Ic8EySRYX@cluster0.bpz97.mongodb.net/merng?retryWrites=true&w=majority'
+    MONGODB: 'mongodb+srv://fadil:TFM3UH0Ic8EySRYX@cluster0.bpz97.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'dadaferertewtgfet'
 }
 
